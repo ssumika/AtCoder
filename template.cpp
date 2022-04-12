18fa905b717f17a07c3059851ca5a3ff9c804e87
ssumika <gsuzuki221@gmail.com>
2022-04-13 01:10:57 +0900

    templete

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<map>
+#include<unordered_set>
+#include<set>
+#include<queue>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> x(n);
+    vector<int> y(n);
+    for(int i=0; i<n; i++){
+        cin>>x[i]>>y[i];
+    }
+}
