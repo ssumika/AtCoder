911c11550c615f13fd532f8254305e99c17da1a6
ssumika <gsuzuki221@gmail.com>
2022-11-14 19:12:15 +0900

    c問題解答訂正

--- abc277/test3.cpp
+++ abc277/test3.cpp
@@ -14,18 +14,6 @@ using ll=long long;
 
 #define mod 998244353
 
-static map<int,bool> seen;
-int ans=1;
-//seenはassign(n,false)等で初期化,Graph G(n)
-void dfs(map<int,vector<int>> &G,int v){
-    seen[v]=true;
-    for(int next_v:G[v]){
-        ans=max(ans,next_v);
-        if(seen[next_v]) continue;
-        dfs(G,next_v);
-    }
-}
-
 int main() {
 	int n;
 	cin>>n;
@@ -35,11 +23,21 @@ int main() {
 		cin>>a>>b;
 		G[a].push_back(b);
 		G[b].push_back(a);
-        seen[a]=false;
-        seen[b]=false;
 	}
-
-    dfs(G,1);
-    cout<<ans<<endl;
+    
+    queue<int> que;
+	que.push(1);
+	set<int> S;
+	S.insert(1);
+	while (!que.empty()) {
+		int v = que.front(); que.pop();
+		for (int i : G[v]) {
+			if (!S.count(i)) {
+				que.push(i);
+				S.insert(i);
+			}
+		}
+	}
+	cout << *S.rbegin() << '\n';
 	
 }
